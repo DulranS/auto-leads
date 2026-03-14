df239c7cab1a0d5cd3957d2942ba33e1b0b232bf
DulranS <127298642+DulranS@users.noreply.github.com>
2026-03-14 22:44:33 +0530

    jghj

--- scrape-mails/app/layout.js
+++ scrape-mails/app/layout.js
@@ -4,6 +4,9 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://accounts.google.com/gsi/client" />
+      </head>
       <body>{children}</body>
     </html>
   );
