7a93317da74672c0a83ca86554c1fe9656ac275b
DulranS <127298642+DulranS@users.noreply.github.com>
2026-03-28 15:00:07 +0530

    kmkm

--- scrape-mails/app/layout.js
+++ scrape-mails/app/layout.js
@@ -6,9 +6,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script async src="https://accounts.google.com/gsi/client" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            // Block extension interference
+            (function() {
+              const originalDefineProperty = Object.defineProperty;
+              Object.defineProperty = function(obj, prop, descriptor) {
+                if (prop === 'ethereum' && window.ethereum) {
+                  return;
+                }
+                return originalDefineProperty.call(this, obj, prop, descriptor);
+              };
+            })();
+          `
+        }} />
       </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>JavaScript must be enabled to use this application</h1>
+          </div>
+        </noscript>
         <ExtensionCleaner />
         {children}
       </body>
